8ff4cbdd6c61a04a33ba3120862bfbb265daecc7
Amro Diab <adiab@hotmail.co.uk>
2016-05-04 17:59:33 +0100

    Only backup file if it is not a symlink

--- scripts/create_simlinks.py
+++ scripts/create_simlinks.py
@@ -18,15 +18,16 @@ DATA = json.loads(RAW)
 for key in DATA.keys():
     source = os.path.abspath('../{}'.format(key))
     DEST = '{}/{}'.format(HOME, DATA[key])
-    print("Creating symlink {}".format(key))
     print('dest: {}'.format(DEST))
 
-    if os.path.exists(DEST):
+    if os.path.islink(DEST):
+        print('{} is already a link, skipping'.format(DEST))
+        continue
+    elif os.path.exists(DEST):
         print('renaming to {}.{}'.format(DEST, CTIME))
         os.rename(DEST, '{}.{}'.format(DEST, CTIME))
-    elif os.path.islink(DEST):
-        os.unlink(DEST)
     try:
+        print("Creating symlink {}".format(key))
         os.symlink(source, DEST)
     except Exception as e:
         print('Failed: ../{} to {}: {}'.format(source, DEST, e))
