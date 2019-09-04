143f734d4914b33ab94ace8708ee5bb4fb32c94f
Amro Diab <adiab@hotmail.co.uk>
2019-09-04 15:54:05 +0100

    bash completion improvments

--- config/bash_completion.d/ssh-completion.sh
+++ config/bash_completion.d/ssh-completion.sh
@@ -4,14 +4,10 @@ _ssh()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts=$(grep '^host' ~/.ssh/config ~/.ssh/config.d/* 2>/dev/null | grep -v '[?*]' | cut -d ' ' -f 2-)
+    opts=$(grep -i '^host' ~/.ssh/config ~/.ssh/config.d/* 2>/dev/null | grep -v '[?*]' | cut -d ' ' -f 2-)
 
     COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
     return 0
 }
-complete -F _ssh ssh
 
-complete -W "$(cat ~/.hosts|awk {'print $2'})" ssh
-complete -W "$(cat ~/.hosts|awk {'print $2'})" scp
-complete -W "$(cat ~/.hosts|awk {'print $2'})" ping
-complete -W "$(cat ~/.hosts|awk {'print $2'})" aws_lookup
+complete -F _ssh -W "$(cat ~/.hosts|awk {'print $2'})" ssh scp ping aws_lookup
